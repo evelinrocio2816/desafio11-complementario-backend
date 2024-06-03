5c8b143bd799ca535fd87075fa0d23c318f67d49
Evelin Romero <Evelinrocio2816@gmail.com>
2024-06-02 21:50:15 -0300

    bd

--- src/db/database.js
+++ src/db/database.js
@@ -1,36 +1,25 @@
+
 const mongoose = require("mongoose");
 const { mongo_url } = require("../config/config");
 const { log } = require("winston");
 
+
 class BaseDatos {
     static #instance;
 
-    constructor() {
-        mongoose.connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
-            .then(() => {
-                log.info("Conexión exitosa a MongoDB");
-            })
-            .catch((error) => {
-                log.error("Error al conectar a MongoDB:", error);
-            });
+    constructor(){
+        mongoose.connect(mongo_url)
     }
-
-    static getInstance() {
-        if (this.#instance) {
-            log.info("Instancia previa de la base de datos encontrada");
-            return this.#instance;
+    static getInstance(){
+        if( this.#instance){
+            console.log("Conexion Previa");
+            return this.#instance
         }
-        this.#instance = new BaseDatos();
-        log.info("Nueva instancia de la base de datos creada");
-        return this.#instance;
-    }
-
-    static cerrarConexion() {
-        if (this.#instance) {
-            mongoose.disconnect();
-            log.info("Conexión a MongoDB cerrada");
-        }
+        this.#instance = new BaseDatos()
+            console.log("Conexion Exitosa!!");
+        return this.#instance
     }
 }
+ module.exports = BaseDatos.getInstance();
 
-module.exports = BaseDatos.getInstance();
+
